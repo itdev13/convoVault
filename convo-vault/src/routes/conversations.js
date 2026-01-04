a552f9b4b3fa8c339641fe4cff4f0ea992d27a23
varaprasad <varaprasad.prasad@gohighlevel.com>
2026-01-04 12:19:45 +0530

    changes

--- convo-vault/src/routes/conversations.js
+++ convo-vault/src/routes/conversations.js
@@ -115,8 +115,8 @@ router.get('/download', authenticateSession, async (req, res) => {
           dateAdded: conv.dateAdded,
           dateUpdated: conv.dateUpdated,
           userId: conv.userId,
-          contactEmail: conv.email,
-          contactPhone: conv.phone,
+          email: conv.email,
+          phone: conv.phone,
         }))
       },
       meta: {
