8bc93c88de35b48f7a31c426c75f7d296f99fad3
varaprasad <jvvprasad04@gmail.com>
2026-03-27 02:15:09 +0530

    fix

--- convo-vault/src/routes/oauth.js
+++ convo-vault/src/routes/oauth.js
@@ -11,7 +11,6 @@ const { logError } = require('../utils/errorLogger');
 const INTERNAL_TESTING_COMPANY_IDS = [
   'PG9VJ27QFRumQrOGB2Ee',
   '7IlT9P1bafOCnq2JV00t',
-  "DVFsQQdD9VqtSc4r4Quq"
 ];
 
 /**
