4b9e2b4ca3082e6e321cb007e8ebdde606d155b2
varaprasad <varaprasad.prasad@gohighlevel.com>
2026-05-10 20:38:05 +0530

    fix

--- convo-vault/src/models/BillingTransaction.js
+++ convo-vault/src/models/BillingTransaction.js
@@ -19,7 +19,7 @@ const billingTransactionSchema = new mongoose.Schema({
   // Transaction type
   type: {
     type: String,
-    enum: ['export_conversations', 'export_messages', 'export_notes', 'export_tasks', 'export_opportunities', 'export_formSubmissions', 'export_links', 'export_socialPosts', 'export_callLogs', 'export_templates', 'export_specialTabMessages', 'export_callTranscriptions', 'export_contacts', 'custom_charge', 'import_notes', 'import_contacts'],
+    enum: ['export_conversations', 'export_messages', 'export_notes', 'export_tasks', 'export_opportunities', 'export_formSubmissions', 'export_links', 'export_socialPosts', 'export_callLogs', 'export_templates', 'export_specialTabMessages', 'export_callTranscriptions', 'export_contacts', 'export_customFields', 'export_customValues', 'export_tags', 'custom_charge', 'import_notes', 'import_contacts', 'import_custom_fields', 'import_custom_values'],
     required: true
   },
 
